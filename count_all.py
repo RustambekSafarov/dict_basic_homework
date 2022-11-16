4dc673af709f6ef555dc70fabff7e19ac270f811
RustambekSafarov <rustambeksafarovv@gmail.com>
2022-11-16 20:03:23 +0500

    done

--- count_all.py
+++ count_all.py
@@ -7,4 +7,17 @@ def count_all(txt):
     Returns:
         dict: dictionary with letters and digits
     """
-    return
+    d = 0
+    l = 0
+    di = {}
+    for i in txt:
+        print(i)
+        if i.isdigit():
+            d+=1
+        
+        elif i!=' ' and not i.isdigit():
+            l += 1
+    di['LETTERS'] = l
+    di['DIGITS'] = d
+    return di
+print(count_all("Hello World"))
